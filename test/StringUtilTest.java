7763b2026d438b0bec9dbac5ac3eb0d9cdd88d2a
Thaksina <thaksina.j@ku.th>
2018-09-05 22:25:29 +0700

    Fixed test

--- test/StringUtilTest.java
+++ test/StringUtilTest.java
@@ -19,7 +19,7 @@ public class StringUtilTest {
 	}
 
 	@Test
-	public void moreTestIndexOf(){
+	public void testReturnTheFirstFound(){
 
 		String[] animal = {"Cat", "Dog", "Dog","Bat"};
 		assertEquals(1, StringUtil.indexOf("Dog", animal));
@@ -27,10 +27,20 @@ public class StringUtilTest {
 	}
 
 	@Test
-	public void moreTestIndexOf2(){
+	public void testnoElementIndexOf(){
 
 		String[] color = {"Blue", "Red", "Yellow","Pink"};
 		assertEquals(-1, StringUtil.indexOf("Green", color));
+	}
+
+
+
+	@Test
+	public void testUpperLetter (){
+
+		String[] color = {"Blue", "Red", "Green","Pink"};
+		assertEquals(2, StringUtil.indexOf("GREEN", color));
+
 	}
 
 	//TODO write more tests. Test other cases.
